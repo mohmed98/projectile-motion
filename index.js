3a02b923b9b124397d9a888157f557c69f61430a
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-05-02 12:37:41 +0200

    stable

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ launchBtn.addEventListener("click", () => {
   const obAngle = document.querySelector("#obAngle").value;
   const obVelocity = document.querySelector("#obVelocity").value;
   const movingObject = document.querySelector("#movingObject");
+  const lineCanvas = document.querySelector("#lineCanvas");
+  const ballCanvas = document.querySelector("#ballCanvas");
+
+  ballCanvas;
   const obPositions = [];
   let t = 0;
 
@@ -17,7 +21,6 @@ launchBtn.addEventListener("click", () => {
       obVelocity * t * Math.sin(Math.PI * (obAngle / 180)) -
       0.5 * 9.8 * Math.pow(t, 2);
     obPositions.push({ x, y });
-    console.log({ y, t });
     if (y < 0) {
       console.log("break");
       break;
@@ -27,6 +30,14 @@ launchBtn.addEventListener("click", () => {
   var i = 0;
   const arrayLen = obPositions.length - 1;
   movingObject.style.transform = `translate(${obPositions[i].x}px, ${obPositions[i].y}px)`;
+
+  const linePath = lineCanvas.getContext("2d");
+  const movingBall = ballCanvas.getContext("2d");
+  movingBall.beginPath();
+  movingBall.arc(50, 150, 10, 0, 2 * Math.PI);
+  movingBall.stroke();
+  linePath.moveTo(0, 150);
+
   movingObject.addEventListener("transitionrun", () => {
     if (i < arrayLen) {
       i++;
@@ -36,7 +47,20 @@ launchBtn.addEventListener("click", () => {
   movingObject.addEventListener("transitionend", () => {
     if (i < arrayLen) {
       movingObject.style.transform = `translate(${obPositions[i].x}px, -${obPositions[i].y}px)`;
+      linePath.lineTo(obPositions[i].x, 150 - obPositions[i].y);
+      console.log(obPositions[i].x, obPositions[i].y) + 100;
+      linePath.stroke();
+      movingBall.clearRect(0, 0, 150, 150);
+      movingBall.beginPath();
+      movingBall.arc(
+        obPositions[i].x,
+        150 - obPositions[i].y,
+        10,
+        0,
+        2 * Math.PI
+      );
+      movingBall.fillStyle = "#000";
+      movingBall.fill();
     }
-    console.log({ arrayLen, i });
   });
 });
